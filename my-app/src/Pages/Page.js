7858bf8ceb42b37634f6c158971443d18f00d68f
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-02-12 16:42:29 -0800

    create initial dashboard components

--- my-app/src/Pages/Page.js
+++ my-app/src/Pages/Page.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from '../Navigation/Header.js';
+import Stat from '../Dashboard/Stat.js';
+import DashboardChart from '../Dashboard/DashboardChart.js';
 
 class Page extends React.Component {
     render() {
         return (
             <div>
-                <h1>Page</h1>
+                <Header></Header>
+                <h1>Dashboard</h1>
+                <Stat></Stat>
+                <DashboardChart></DashboardChart>
             </div>
         );
     }
