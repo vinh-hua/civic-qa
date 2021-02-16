cae3607c3bc2920f15f4487b2acf23253da5d294
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-02-16 15:56:06 -0800

    convert to functional components

--- my-app/src/Dashboard/DashboardChart.js
+++ my-app/src/Dashboard/DashboardChart.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Chart } from 'react-charts';
 
-class DashboardChart extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Today's trends</h1>
-            </div>
-        );
-    }
+const DashboardChart = () => {
+    return (
+        <div>
+            <h1>Today's trends</h1>
+        </div>
+    );
 }
 
 export default DashboardChart;
--- my-app/src/Navigation/Header.js
+++ my-app/src/Navigation/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Civic Q&A</h1>
-            </div>
-        );
-    }
+const Header = () => {
+    return (
+        <div>
+            <h1>Civic Q&A</h1>
+        </div>
+    );
 }
 
 export default Header;
