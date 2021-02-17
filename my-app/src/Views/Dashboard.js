ee43080ad555bd6c535946074c68e2b3cb3f4c26
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-02-17 02:38:26 -0800

    initial line chart

--- my-app/src/Views/Dashboard.js
+++ my-app/src/Views/Dashboard.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import StatCard from '../Dashboard/StatCard.js';
+import './Dashboard.css';
 
-// has placeholder values for StatCards
+// currently using dummy data for StatCards and LineChart
 function Dashboard() {
+    const test_data = [{name: 1, uv: 12},
+                       {name: 2, uv: 15},
+                       {name: 3, uv: 34}];
+    const renderLineChart = (
+        <div class="chart">
+            <LineChart width={700} height={300} data={test_data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+        </div>
+
+    );
     return (
         <div>
-            <h1>Dashboard</h1>
             <StatCard title={"General"} data={625}></StatCard>
             <StatCard title={"Casework"} data={198}></StatCard>
             <StatCard title={"Assigned"} data={190}></StatCard>
             <StatCard title={"Overdue"} data={246}></StatCard>
+            {renderLineChart}
         </div>
     );
 }
