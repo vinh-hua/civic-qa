74cd65eac2ae2e4d0cf4c65869a2445ba666515c
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-03-01 21:28:13 -0800

    create initial bar chart for issues and inquiries

--- my-app/src/Views/EngagementReports.tsx
+++ my-app/src/Views/EngagementReports.tsx
@@ -1,10 +1,67 @@
 import { Header } from '../Components/Header';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { SubHeaderLine } from '../Components/SubHeaderLine';
+
+export type EngagementReportBarChartData = {
+    name: string;
+    count: number;
+}
+
 
 export function EngagementReports() {
+    const test_data = [
+        {
+          name: 'Topic 1',
+          count: 250
+        },
+        {
+          name: 'Topic 2',
+          count: 170
+        },
+        {
+          name: 'Topic 3',
+          count: 192
+        },
+        {
+          name: 'Topic 4',
+          count: 32
+        },
+        {
+          name: 'Topic 5',
+          count: 45
+        }
+      ];
+
+    var sorted_data: EngagementReportBarChartData[] = test_data.sort((a, b) => {
+        if (a.count > b.count) {
+            return -1;
+        } else if (a.count < b.count) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
     return (
         <div className="dashboard sub-dashboard">
             <Header title="Engagement Reports"></Header>
+            <h2>Issues/Inquiries</h2>
+            <BarChart
+                width={700}
+                height={500}
+                data={test_data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="count" fill="#855CF8" />
+            </BarChart>
         </div>
     );
 }
