0b67754a723abaf5d6babf9f4c24c04f661caec5
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-03-05 13:16:12 -0800

    styling form response

--- my-app/src/Views/Responses.tsx
+++ my-app/src/Views/Responses.tsx
@@ -4,6 +4,7 @@ import { SubDashboard, SubDashboardData } from '../Components/SubDashboard';
 import { SpecificView } from '../Components/SpecificView'; 
 
 // currently using test data
+// TODO: sort by time ?? backend feature??
 function getSubDashboardData(): Array<SubDashboardData> {
     var data = [];
     data.push({name: "Lucille Harmon / COVID-19 Stimulus", value: "4:35pm"});
@@ -21,7 +22,7 @@ function getSubDashboardData(): Array<SubDashboardData> {
 
 export function Responses() {
     const test_data = getSubDashboardData();
-    const test_body = "Dear WA 36th Legislative Staff, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus diam egestas augue dignissim, quis accumsan tortor pulvinar. Suspendisse mattis quam magna, ut dapibus leo volutpat non. Donec sapien mauris, semper non odio at, gravida posuere massa. Sed mattis diam id sapien semper sodales. Nam in justo ultrices, facilisis arcu vitae, ornare velit. Nam vitae aliquam...";
+    const test_body = "Dear WA 36th Legislative Staff, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus diam egestas augue dignissim, quis accumsan tortor pulvinar. Suspendisse mattis quam magna, ut dapibus leo volutpat non. Donec sapien mauris, semper non odio at, gravida posuere massa. Sed mattis diam id sapien semper sodales. Nam in justo ultrices, facilisis arcu vitae, ornare velit. Nam vitae aliquam... More text to test overflow, is it overflowing, is it overflowing, is it overflowing, is it overflowing, is it overflowing, is it overflowing, is it overflowing?";
     const [onResponseView, setResponseView] = useState(false);
     const [responseSubject, setResponseSubject] = useState("");
     const [responseBody, setResponseBody] = useState("");
