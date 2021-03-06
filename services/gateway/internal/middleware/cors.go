f8b90b9d9a6d121cc0c60370c61cc1652ab3ab2c
Rafael Bayer <rafibayer7@gmail.com>
2021-03-05 16:52:54 -0800

    added cors middleware and enabled it on gateway

---
+++ services/gateway/internal/middleware/cors.go
@@ -0,0 +1,22 @@
+package middleware
+
+import "net/http"
+
+// CorsMiddleware adds permissive CORS settings to all incoming requests
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+		w.Header().Set("Access-Control-Max-Age", "600")
+
+		// Handles pre-flight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
