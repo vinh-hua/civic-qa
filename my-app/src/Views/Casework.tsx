8b22d4952891abdd705511ba5e94a7dc6b1a5757
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-03-07 15:45:12 -0800

    update casework to have specific views

--- my-app/src/Views/Casework.tsx
+++ my-app/src/Views/Casework.tsx
@@ -3,6 +3,7 @@ import { Header } from '../Components/Header';
 import { SubDashboard, SubDashboardData } from '../Components/SubDashboard';
 import { SubHeaderLine } from '../Components/SubHeaderLine';
 import { StatCardRow } from '../Components/StatCardRow';
+import { Responses } from './Responses';
 
 // TODO: will data be pre-sorted on back-end?
 // currently using test data
@@ -16,8 +17,18 @@ function getSubDashboardData(): Array<SubDashboardData> {
     return data as Array<SubDashboardData>;
 }
 
+function getSpecificData(): Array<SubDashboardData> {
+    var data = [];
+    data.push({name: "Test", value: 123});
+    data.push({name: "Test", value: 123});
+    data.push({name: "Test", value: 123});
+    data.push({name: "Test", value: 123});
+    return data as Array<SubDashboardData>;
+}
+
 export function Casework() {
-    const test_data = getSubDashboardData();
+    const testData = getSubDashboardData();
+    const testSpecificData = getSpecificData();
     const [onSpecificView, setSpecificView] = useState(false);
 
     let statCards = [
@@ -26,13 +37,30 @@ export function Casework() {
         {title: "Topics", stat: 10}
     ];
 
+    function specificView() {
+        setSpecificView(true);
+    }
+
+    function initialView() {
+        setSpecificView(false);
+    }
+
     return (
-        <div className="dashboard sub-dashboard">
-            <Header title="Casework"></Header>
-            <SubDashboard title="TOPIC" data={test_data} changeViewFunc={setSpecificView} emailTemplates={false} fullPageView={false}></SubDashboard>
-            <div className="sub-summary">
-                <SubHeaderLine title="SUMMARY"></SubHeaderLine>
-                <StatCardRow cards={statCards}></StatCardRow>
+        onSpecificView ? 
+        <div> 
+            <div className="dashboard sub-dashboard">
+                <button className="exit-button" onClick={initialView}><img src="./assets/icons/back-arrow.png"></img></button>
+            </div>
+            <Responses header="Casework" data={testSpecificData}></Responses>
+        </div>
+        : <div className="dashboard sub-dashboard">
+            <div>
+                <Header title="Casework"></Header>
+                <SubDashboard title="TOPIC" data={testData} changeViewFunc={specificView} emailTemplates={false} fullPageView={false}></SubDashboard>
+                <div className="sub-summary">
+                    <SubHeaderLine title="SUMMARY"></SubHeaderLine>
+                    <StatCardRow cards={statCards}></StatCardRow>
+                </div>
             </div>
         </div>
     );
