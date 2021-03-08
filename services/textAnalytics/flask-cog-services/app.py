bd5e2c7d781404bd9453ad9f2592adaf844a0fa1
Amit <amit.galitzky@live.com>
2021-03-08 15:49:50 -0800

    cleaned up some code and added an extrat personal info api call

--- services/textAnalytics/flask-cog-services/app.py
+++ services/textAnalytics/flask-cog-services/app.py
@@ -3,30 +3,40 @@ import textAnalytics
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-articles = [
-    """
-    Washington, D.C. Autumn in DC is a uniquely beautiful season. The leaves fall from the trees
-    in a city chockful of forrests, leaving yellow leaves on the ground and a clearer view of the
-    blue sky above...
-    """,
-    """
-    Redmond, WA. In the past few days, Microsoft has decided to further postpone the start date of
-    its United States workers, due to the pandemic that rages with no end in sight...
-    """,
-    """
-    Redmond, WA. Employees at Microsoft can be excited about the new coffee shop that will open on campus
-    once workers no longer have to work remotely...
-    """
-]
+# articles = [
+#     """
+#     Washington, D.C. Autumn in DC is a uniquely beautiful season. The leaves fall from the trees
+#     in a city chockful of forrests, leaving yellow leaves on the ground and a clearer view of the
+#     blue sky above...
+#     """,
+#     """
+#     Redmond, WA. In the past few days, Microsoft has decided to further postpone the start date of
+#     its United States workers, due to the pandemic that rages with no end in sight...
+#     """,
+#     """
+#     Redmond, WA. Employees at Microsoft can be excited about the new coffee shop that will open on campus
+#     once workers no longer have to work remotely...
+#     """
+# ]
+# documents = [
+#     "The employee's SSN is 859-98-0987.",
+#     "The employee's phone number is 555-555-5555."
+# ]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/key-phrase-extraction', methods=['POST'])
-def extract_key_phrases():
+@app.route('/key-phrase', methods=['POST'])
+def extract_key_phrases_api():
     data = request.get_json()
-    print("data" , data)
     text_input = data['body']
     response = textAnalytics.extract_key_phrases(text_input)
+    return jsonify(response)
+
+@app.route('/pii-entities', methods=['POST'])
+def extract_pii_entities_api():
+    data = request.get_json()
+    text_input = data['body']
+    response = textAnalytics.extract_pii_entities(text_input)
     return jsonify(response)
--- services/textAnalytics/flask-cog-services/textAnalytics.py
+++ services/textAnalytics/flask-cog-services/textAnalytics.py
@@ -1,43 +1,40 @@
 import os
 import sys
 
-def extract_key_phrases(input):
-    print(
-        "In this sample, we want to find the articles that mention Microsoft to read."
-    )
-    # [START extract_key_phrases]
+def authenticate_client():
     from azure.core.credentials import AzureKeyCredential
     from azure.ai.textanalytics import TextAnalyticsClient
 
     endpoint = os.environ["AZURE_TEXT_ANALYTICS_ENDPOINT"]
     key = os.environ["COGNITIVE_SERVICE_KEY"]
-    
-
     text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
-    articles = [
-        """
-        Washington, D.C. Autumn in DC is a uniquely beautiful season. The leaves fall from the trees
-        in a city chockful of forrests, leaving yellow leaves on the ground and a clearer view of the
-        blue sky above...
-        """,
-        """
-        Redmond, WA. In the past few days, Microsoft has decided to further postpone the start date of
-        its United States workers, due to the pandemic that rages with no end in sight...
-        """,
-        """
-        Redmond, WA. Employees at Microsoft can be excited about the new coffee shop that will open on campus
-        once workers no longer have to work remotely...
-        """
-    ]
+    return text_analytics_client
 
-    result = text_analytics_client.extract_key_phrases(articles)
+def extract_key_phrases(input):
+    print(
+        "In this sample, we want to find the articles that mention Microsoft to read."
+    )
+    client = authenticate_client()
+    result = client.extract_key_phrases(input)
     print(result, file=sys.stderr)
     resultMap = {}
     for idx, doc in enumerate(result):
         if not doc.is_error:
             resultMap[idx] = doc.key_phrases
-            # print("Key phrases in article #{}: {}".format(
-            #     idx + 1,
-            #     ", ".join(doc.key_phrases)
-            # ))
     return resultMap
+
+def extract_pii_entities(input):
+    client = authenticate_client()
+    response = client.recognize_pii_entities(input, language="en")
+    result = [doc for doc in response if not doc.is_error]
+    personal_info = {}
+    for idx, doc in enumerate(result):
+        list_of_entities = []
+        for entity in doc.entities:
+            entity_map = {}
+            entity_map["text"] = entity.text
+            entity_map["category"] = entity.category
+            entity_map["confidence_score"] = entity.confidence_score
+            list_of_entities.append(entity_map)
+        personal_info[idx] = list_of_entities
+    return personal_info
