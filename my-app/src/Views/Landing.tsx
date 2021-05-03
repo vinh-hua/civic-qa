3e4ad2fc9567636c23d6c32d70fc69fcdedf238e
Vivian Hua <vivianhua@Vivians-MacBook-Pro.local>
2021-05-03 15:43:47 -0700

    styling landing page

--- my-app/src/Views/Landing.tsx
+++ my-app/src/Views/Landing.tsx
@@ -6,28 +6,29 @@ import './Landing.css';
 export function Landing() {
     return(
         <div className="landing-page">
-            <div className="landing-logo-title-container">
-                <img className="landing-logo" src="./assets/icons/logo.png"></img>
-                <h1 className="landing-title">Civic QA</h1>
-            </div>
-            <div className="landing-btn-container">
-                <NavLink className="landing-btn" to="/signup">Signup</NavLink>
-                <NavLink className="landing-btn" to="login">Login</NavLink>
+            <div className="landing-page-bar">
+                <div className="landing-logo-title-container">
+                    <img className="landing-logo" src="./assets/icons/logo.png"></img>
+                    <h1 className="landing-title">Civic QA</h1>
+                </div>
+                <div className="landing-btn-container">
+                    <NavLink className="landing-btn" to="/signup">Signup</NavLink>
+                    <NavLink className="landing-btn" to="login">Login</NavLink>
+                </div>
             </div>
             <div className="project-container">
-                <div className="project-overview">
-                    <h1 className="landing-sub-title">Civic QA Overview</h1>
-                    <p>{LandingPage.ProjectOverview1}</p>
-                    <h2>Why care?</h2>
-                    <p>{LandingPage.ProjectOverview2}</p>
-                    <p>{LandingPage.ProjectOverview3}</p>
+                <div className="landing-sub-section project-overview">
+                    <h1 className="landing-sub-title">Problem Overview</h1>
+                    <p>{LandingPage.ProblemOverview1}</p>
+                    <p>{LandingPage.ProblemOverview2}</p>
+                    <p>{LandingPage.ProblemOverview3}</p>
                 </div>
-                <div className="stakeholders">
+                <div className="landing-sub-section stakeholders">
                     <h1 className="landing-sub-title">Legislative Assistants</h1>
                     <p>{LandingPage.TargetStakeholders1}</p>
                     <p>{LandingPage.TargetStakeholders2}</p>
                 </div>
-                <div className="project-solution">
+                <div className="landing-sub-section project-solution">
                     <h1 className="landing-sub-title">How does Civic QA help?</h1>
                     <p>{LandingPage.Solution1}</p>
                     <ol className="process-steps">
@@ -40,7 +41,11 @@ export function Landing() {
                     <p>{LandingPage.Solution2}</p>
                     <p>{LandingPage.Solution3}</p>
                 </div>
-                <div className="about-team">
+                <div className="landing-sub-section presentation-video">
+                    <h1 className="landing-sub-title">Presentation Video</h1>
+                    <iframe height="500" width="800" src="https://www.youtube.com/embed/COTimhg6Cs8"></iframe>
+                </div>
+                <div className="landing-sub-section about-team">
                     <h1 className="landing-sub-title">Meet the Team</h1>
                     <div className="team-bios">   
                         <TeamCard name="Lia Kitahata" img="./assets/images/lia.jpg" bio={LandingPage.LiaBio}></TeamCard>
