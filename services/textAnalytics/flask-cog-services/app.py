b720fd7b95c34f7ef6183f4c0096a75f0dcc5e50
Amit <amit.galitzky@live.com>
2021-05-13 23:11:41 -0700

    rake nltk controller added

--- services/textAnalytics/flask-cog-services/app.py
+++ services/textAnalytics/flask-cog-services/app.py
@@ -4,6 +4,7 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from controller.controller import Controller
 from analytics.textAnalytics import AzureAnalytics
 from analytics.mockAnalytics import MockAnalytics
+from analytics.rakeAnalytics import RakeAnalytics
 import os
 import sys
 
@@ -14,7 +15,7 @@ NUM_TAGS = 3
 
 # need to add logging aggregator request
 
-analytics_impl = os.getenv("ANALYTICS_IMPL", "mock")
+analytics_impl = os.getenv("ANALYTICS_IMPL", "rake")
 analyticsController = None
 
 if analytics_impl == "azure":
@@ -34,6 +35,10 @@ if analytics_impl == "azure":
 if analytics_impl == "mock":
     analyticsController = Controller(MockAnalytics())
     print("Mock analytics")
+
+if analytics_impl == "rake":
+    analyticsController = Controller(RakeAnalytics())
+    print("Rake analytics")
 
 
 app = Flask(__name__)
@@ -58,20 +63,6 @@ def extract_key_phrases_api():
         print(e.with_traceback(), file=sys.stderr)
         data = {'error': 'InternalServerError from Azure Analytics API - keyphrase'}
         return jsonify(data), 500
-
-# @app.route(f'{VERSION_BASE}/pii-entities', methods=['POST'])
-# def extract_pii_entities_api():
-
-#     data = request.get_json()
-#     text_input = data['body']
-
-
-#     try: 
-#         resp = textAnalytics.extract_pii_entities(text_input, text_analytics_client)
-#         return jsonify(resp)
-#     except Exception:
-#         data = {'error': 'InternalServerError from Azure Analytics API'}
-#         return jsonify(data), 500
 
 if __name__ == "__main__":
     addr = os.getenv("ADDR", 9090)
